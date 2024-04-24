6d0737c2f8586100ac2cad5b84cf7a30d7df8e40
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-04-24 11:07:54 +0000

    :sparkles: added new code to main

--- setup.sh
+++ setup.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
+## Script para realizar para desabilitar serviços de rede padrão para liberar o acesso a recursos de rede (portas UDP) para os containers Docker:
 
 read -p "Escolha se deseja inicializar (true) ou desativar (false) o appliance ZTP: " opcao
 
 if [[ "$opcao" == "true" ]]; then
+    echo "Desabilitando o DHCP nativo..."
+    sudo systemctl stop systemd-networkd.socket
+    sudo systemctl stop systemd-networkd
+    echo "Verificando status dos serviços:"
+    sudo systemctl status systemd-networkd.socket
+    sudo systemctl status systemd-networkd
+    echo "verificando status das portas:"
+    sudo ss -lnu
     echo "Realizando o build do appliance ZTP..."
     docker compose build
     sleep 10
-    echo "Desabilitando o DHCP nativo..."
-     sudo systemctl stop systemd-networkd.socket
-     sudo systemctl stop systemd-networkd
-    echo "Verificando status dos serviços:"
-     sudo systemctl status systemd-networkd.socket
-     sudo systemctl status systemd-networkd
-    echo "verificando status das portas:"
-     sudo ss -lnu 
     echo "Iniciando o appliance ZTP..."
-    docker compose up & 
+    docker compose up &
 elif [[ "$opcao" == "false" ]]; then
     echo "Deligando o appliance ZTP..."
     docker compose down
 
     echo "Habilitando o DHCP nativo..."
-     sudo systemctl start systemd-networkd.socket
-     sudo systemctl start systemd-networkd
+    sudo systemctl start systemd-networkd.socket
+    sudo systemctl start systemd-networkd
 
     echo "Verificando status dos serviços:"
-     sudo systemctl status systemd-networkd.socket
-     sudo systemctl status systemd-networkd
-     sudo netplan apply
-     sudo ss -lnu 
+    sudo systemctl status systemd-networkd.socket
+    sudo systemctl status systemd-networkd
+    sudo netplan apply
+    sudo ss -lnu
 else
     echo "Opção inválida. Use 'true' para habilitar ou 'false' para desabilitar o appliance ZTP."
 fi
