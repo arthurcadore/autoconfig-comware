c200b6022f7d1b2e641425235926659618634db2
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-04-29 18:26:26 -0300

    :sparkles: updated list of hosts for playbook

--- share/playbooks/playbooks.sh
+++ share/playbooks/playbooks.sh
@@ -9,7 +9,7 @@ while true; do
     echo ""
 
     ## Execute o playbook de exemplo do Ansible
-    ansible-playbook -i inventory/hosts runner.yaml
+    ansible-playbook -i inventory/hosts.yaml runner.yaml
     ## Aguarde um tempo antes de iniciar a próxima execução (opcional)
     sleep 5
 done
