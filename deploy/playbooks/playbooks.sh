9b3427d9810c42363e33c7aa38166cddb537d597
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-09-02 09:07:48 -0300

    :sparkles: created files for Ansible

---
+++ deploy/playbooks/playbooks.sh
@@ -0,0 +1,4 @@
+## Execute Ansible example playbooks
+ansible-playbook -i inventory/inventory.yaml playbooks/playbook1.yaml
+ansible-playbook -i inventory/inventory.yaml playbooks/playbook2.yaml
+ansible-playbook -i inventory/inventory.yaml playbooks/playbook3.yaml
