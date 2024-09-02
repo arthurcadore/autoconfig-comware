50e7a59c609c10652dd19c3633ffd149dc8b7553
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-09-02 12:15:35 -0300

    :sparkles: added new configs to main

--- deploy/playbooks/playbooks.sh
+++ deploy/playbooks/playbooks.sh
@@ -1,13 +1,13 @@
 ## Execute Ansible example playbooks
 
 # Firmware Upgrade: 
-ansible-playbook -i inventory/inventory.yaml playbooks/updateFirmware.yaml
+ansible-playbook -i inventory/inventory.yaml playbooks/updateFirmware.yaml -v
 
 # Startup Config file Update: 
-ansible-playbook -i inventory/inventory.yaml playbooks/updateStartup.yaml
+ansible-playbook -i inventory/inventory.yaml playbooks/updateStartup.yaml -v
 
 # Configuration Playbook Examples:
-ansible-playbook -i inventory/inventory.yaml playbooks/configExample1.yaml
-ansible-playbook -i inventory/inventory.yaml playbooks/configExample2.yaml
-ansible-playbook -i inventory/inventory.yaml playbooks/configExample3.yaml
+ansible-playbook -i inventory/inventory.yaml playbooks/configExample1.yaml -v
+ansible-playbook -i inventory/inventory.yaml playbooks/configExample2.yaml -v
+ansible-playbook -i inventory/inventory.yaml playbooks/configExample3.yaml -v
 
